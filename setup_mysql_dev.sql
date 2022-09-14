17e64292522daf2a7707d46fccec08b7df8098c1
root <ahmadabdul592@gmail.com>
2022-09-14 11:02:34 +0000

    Server side Basics

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Prepares the server for these project
+-- Creates a database hbnb_dev_db and a new user use hbnb_dev
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+--create a user and grant privileges
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
