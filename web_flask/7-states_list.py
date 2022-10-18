71fb3c12fe95c690835cf8fdd10ea77b70218e3f
Abdul Ahmed Abdul <34511653+ahmadabdul592@users.noreply.github.com>
2022-10-18 02:43:41 +0100

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""List all states Module"""
+"""Starts a Flask web application"""
 
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    """display a HTML the States"""
-    all_states = list(storage.all(State).values())
-    return (render_template('7-states_list.html', all_states=all_states))
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """Returns a rendered html template
+    at the /states_list route,
+    listing all states"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """function that call close methofd"""
+    """Removes the current SQLAlchemy Session"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
