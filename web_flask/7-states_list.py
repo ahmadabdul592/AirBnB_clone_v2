2b18e3fbd07c21475ed54f07a7e9ba6f4f3c3734
ahmadabdul592 <ahmadabdul592@gmail.com>
2022-10-17 18:25:39 -0700

    web flask implementation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-    Runs a Flask web application on 0.0.0.0:5000
-"""
-from ../models import storage
-from flask import Flask
-from flask import render_template
+"""List all states Module"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """ Returns an HTML page of all States sorted by name """
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
+@app.route('/states_list')
+def states_list():
+    """display a HTML the States"""
+    all_states = list(storage.all(State).values())
+    return (render_template('7-states_list.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """ Removes the current SQLAlchemy session. """
+def teardown(self):
+    """function that call close methofd"""
     storage.close()
 
 
-if __name__ == "__main__":
-    """ Run on 0.0.0.0 """
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
